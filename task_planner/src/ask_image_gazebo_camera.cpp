92eeeff07720d1d2c07ad8b2e90b9701655a5675
matteogrisenti <matteo.grisenti@studenti.unitn.it>
2023-12-26 17:15:31 +0100

    Update ask_image_gazebo_camera.cpp
    Aggiunto subsriber al topic image left

--- task_planner/src/ask_image_gazebo_camera.cpp
+++ task_planner/src/ask_image_gazebo_camera.cpp
@@ -1,21 +1,26 @@
-
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h" // Include il messaggio per la PointCloud2
+#include "sensor_msgs/Image.h"         // Include il messaggio per le Immagini
 
 void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
     // Funzione di callback chiamata ogni volta che arriva un nuovo messaggio sulla point_cloud
-
-    // Qui puoi scrivere la logica per manipolare i dati della point cloud
-    // Nel caso di questo esempio, stampiamo solo il numero di punti nella nuvola
     ROS_INFO("Nuova nuvola di punti ricevuta. Numero di punti: %d", msg->width * msg->height);
 }
 
+void imageCallback(const sensor_msgs::Image::ConstPtr& msg) {
+    // Funzione di callback chiamata ogni volta che arriva un nuovo messaggio sulla point_cloud
+    ROS_INFO("Nuova immagine ricevuta. Dimesnioni : %d, %d", msg->width , msg->height);
+}
+
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "point_cloud_subscriber"); // Inizializza il nodo
+    ros::init(argc, argv, "camera_subscriber"); // Inizializza il nodo
     ros::NodeHandle n; // Crea un handle per il nodo
 
     // Crea un subscriber per il topic della point cloud
-    ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>("/ur5/zed_node/point_cloud/cloud_registered", 10, pointCloudCallback);
+    ros::Subscriber sub1 = n.subscribe<sensor_msgs::PointCloud2>("/ur5/zed_node/point_cloud/cloud_registered", 10, pointCloudCallback);
+
+    // Crea un subscriber per il topic delle immagini 
+    ros::Subscriber sub2 = n.subscribe<sensor_msgs::Image>("/ur5/zed_node/left/image_rect_color", 10, imageCallback);
 
     // Loop di esecuzione per la ricezione dei messaggi
     ros::spin();
