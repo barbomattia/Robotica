4a2fe89b136fbc0eede76459da8e36c210db6fb9
Angelo Nutu <nutuangelo@yahoo.it>
2024-02-09 14:17:16 +0000

    finalized ROS interface

--- vision_planner/src/main.py
+++ vision_planner/src/main.py
@@ -34,6 +34,7 @@ import cv2 as cv
 import sensor_msgs.point_cloud2 as pc2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, PointCloud2
+from vision_planner.srv import objectDetection, objectDetectionResponse
 
 class Vision:
     def __init__(self) -> None:
@@ -42,6 +43,7 @@ class Vision:
         self.results = []
         self.bridge = CvBridge()
         self.nn = dc.loadONNX()
+        ros.loginfo('Opened the neural network')
     
     def convertRosToOpen3D(self):
         fields = [field.name for field in self.pcd.fields]
@@ -55,45 +57,66 @@ class Vision:
     
     def imageCallback(self, _image: Image):
         try:
-            print('Image acquired')
+            ros.loginfo('Image acquired')
             image = self.bridge.imgmsg_to_cv2(_image, 'bgr8')
             self.image = image
-            print('Bridged')
+            ros.loginfo('Bridged to Mat')
         except CvBridgeError as e:
-            ros.logerr(e) 
+            ros.logfatal(e) 
     
     def pcdCallback(self, _pcd: PointCloud2):
-        print('Point cloud acquired')
+        ros.loginfo('Point cloud acquired')
         self.pcd = _pcd
         self.convertRosToOpen3D()
-        print('Point cloud converted')
+        ros.loginfo('Point cloud converted')
 
-    def process(self):
+    def process(self, req):
+        self.imageCallback(ros.wait_for_message('/ur5/zed_node/left/image_rect_color', Image))
+        self.pcdCallback(ros.wait_for_message('/ur5/zed_node/point_cloud/cloud_registered', PointCloud2))
+        
         detections = dc.inference(self.image, self.nn)
-        print('Detected')
-        dc.showBBox(self.image, detections)
+        ros.loginfo('Blocks detected')
+        #dc.showBBox(self.image, detections)
+        
         for detection in detections:
             self.results.append(f'{detection.className}: {detection.bbox}')
-        print('Detections saved')
+        ros.loginfo('Block detections were saved')
+        
         self.pcd = pd.rotate_point_cloud(self.pcd)
-        print('Point cloud rotated')
+        ros.loginfo('Point cloud was rotated')
         intrinsics = o3d.camera.PinholeCameraIntrinsic(width=1920, height=1080, fx=790.94585984335442, fy=790.94585984335442, cx=960, cy=540)
         depth_frame = pd.create_depth_image_from_point_cloud(self.pcd, intrinsics)
         meshes = pd.load_meshes('./models/')
-        print('Meshes loaded')
+        ros.loginfo('Meshes are loaded')
+        
         blocks = pd.crop_depth_image(self.results, depth_frame, intrinsics)
+        ros.loginfo('Cropped the blocks')
         data = pd.find_best(meshes, blocks)
-        for d in data:
-            print(d)
+        ros.loginfo('Pose and center extracted')
+        
+        nameBlocks = []
+        xBlocks = []
+        phiBlocks = []
+        for block in data:
+            ros.loginfo(block)
+            nameBlocks.append(block[0])
+            for pos in block[1]:
+                xBlocks.append(pos)
+            for angle in block[2]:
+                phiBlocks.append(angle)
+            
+        response = objectDetectionResponse()
+        response.xBlocks = xBlocks
+        response.phiBlocks = phiBlocks
+        response.nameBlocks = nameBlocks
+        
+        ros.loginfo('Sending data')
+        return response
 
 if __name__ == '__main__':
     vision = Vision()
     
     ros.init_node('vision', anonymous=True)
-    #ros.Subscriber('/ur5/zed_node/left/image_rect_color', Image, vision.imageCallback, queue_size = 1)
-    #ros.Subscriber('/ur5/zed_node/point_cloud/cloud_registered', PointCloud2, vision.pcdCallback, queue_size = 1)
-    
-    vision.imageCallback(ros.wait_for_message('/ur5/zed_node/left/image_rect_color', Image))
-    vision.pcdCallback(ros.wait_for_message('/ur5/zed_node/point_cloud/cloud_registered', PointCloud2))
-    vision.process()
-    
+    ros.Service('object_detection', objectDetection, vision.process)
+    ros.loginfo('Block Detection service is ready to process.')
+    ros.spin()
